b29806df6f03f354e89f8bf384db04620a7d93a5
mancinoa <alex.mancino@gmail.com>
2019-08-08 13:41:00 -0400

    Added MathApp project

---
+++ MathApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_3C
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            var prg1 = new Program1();
+            prg1.calcT();
+            Console.ReadLine();
+        }
+        public decimal calcT()
+        {
+            int i;
+            decimal t = 6000, m = 6000;
+            for (i = 1; i < 6; i++)
+            {
+                if (i != 1)
+                {
+                    t += (m * .02M);
+                    Console.WriteLine($"For year {i} your tuition will be {t:C}");
+                }
+                else
+                {
+                    Console.WriteLine($"For year {i} your tuition will be {t:C}");
+                }
+            }
+            return t;
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # MSSA_Projects
 Collection of various projects I've made while in MSSA
 
--Manipulating Arrays: 
-Console app showing various array manipulations including rotations,
-sorting, reversing arrays, and finding the mean of an array
+-Manipulating Arrays (8 Aug 19): 
+C# Console app showing various array manipulations including rotations,
+sorting, reversing arrays, and finding the mean of an array.
+
+-MathApp (16 Jul 19):
+C# Console App that takes in two values, an operator, and calculates
+the output. 
